7ee6691810e92430ebcec53634dab77342bf1b70
Daren Yong <daren.yong@sjrb.ca>
2018-06-03 22:58:48 -0600

    fix parse

--- src/client/say-hello.js
+++ src/client/say-hello.js
@@ -12,7 +12,7 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const sayHello = async () => {
   await sleep(2000);
   document.body.appendChild(createMsg(_.join(['Hello', 'webpack'], ' ')));
-  const obj = queryString.parse(location.search);
+  const obj = queryString.parse(location.search, { ignoreQueryPrefix: true });
   console.log('obj', obj);
   document.body.appendChild(createMsg(obj.app));
   document.body.appendChild(createMsg(obj.redirect));
